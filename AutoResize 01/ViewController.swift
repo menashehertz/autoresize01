76bafad744f8d8f30de4d4892624d1530c6c892e
menashehertz <steven.hertz@tech4everyone.com>
2017-02-27 17:38:20 -0500

    Initial Commit

---
+++ AutoResize 01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoResize 01
+//
+//  Created by Steven Hertz on 2/27/17.
+//  Copyright © 2017 t4e. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
